50cd05d958907baf930e88345cf712f02b5379c3
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2022-12-05 21:00:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="m-[20px]">
+    <div class="border border-black w-full h-full p-[16px]">
+      <p class="font-normal text-[20px]">3rd Party Instruction</p>
+      <!-- A -->
+      <div class="grid grid-cols-12 mt-[20px] space-x-[5%] border border-gray-200 p-[16px] rounded-[5px]">
+        <div class="col-span-1">
+          <select name="" id=""
+            class="w-[7vw] border border-gray-300 outline-none bg-transparent rounded-[5px] p-[6px]">
+            <option value="">
+              <img src={{./assets/cargo-truck.png}} class="w-full
+                  h-full" alt="" />
+            </option>
+          </select>
+        </div>
+        <div class="col-span-1">
+          <p class="bg-gray-400 rounded-full w-[6vw] py-[7px] text-center px-[1px]">
+            draft
+          </p>
+          <div>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- A -->
+    </div>
+  </div>
+</template>
